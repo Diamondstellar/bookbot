09944e6b1d355a12734ff75d2b846a296bb62593
Diamondstellar <edwardcats16@gmail.com>
2025-12-27 09:38:57 +0900

    functional bookbot

--- main.py
+++ main.py
@@ -8,8 +8,22 @@ def get_book():
     text = get_book_text("books/frankenstein.txt")
     return text
 
+def turn_list_to_string(lis):
+    char_string = ""
+    for dic in lis:
+        if dic["char"].isalpha():
+            char_string += dic["char"] + ":" + " " + str(dic["num"]) + "\n"
+    return char_string
+
 book = get_book()
 wc = get_word_count(book)
 cc = sorted_list(character_count(book))
+lc = turn_list_to_string(cc)
 
-print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\n{wc}\n--------- Character Count -------\n")
+print(f"============ BOOKBOT ============\n\
+Analyzing book found at books/frankenstein.txt...\n\
+----------- Word Count ----------\n\
+{wc}\n\
+--------- Character Count -------\n\
+{lc}\
+============= END ===============")
